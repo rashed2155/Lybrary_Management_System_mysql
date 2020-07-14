1dbf1a7e0ce2d6b4c8332a745082d941263f1d00
rashed2155 <68286834+rashed2155@users.noreply.github.com>
2020-07-14 20:26:59 +0600

    Initial commit

---
+++ src/Library_Management/My_CNX.java
@@ -0,0 +1,43 @@
+package Library_Management;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.*;
+
+/**
+ *
+ * @author 1BestCsharp
+ */
+public class My_CNX {
+
+    private static String servername = "localhost";
+    private static String username = "root";
+    private static String dbname  = "library";
+    private static Integer portnumber  = 3306;
+    private static String password = "";
+    
+    public static Connection getConnection()
+    {
+        Connection cnx = null;
+        
+        MysqlDataSource datasource = new MysqlDataSource();
+        
+        datasource.setServerName(servername);
+        datasource.setUser(username);
+        datasource.setPassword(password);
+        datasource.setDatabaseName(dbname);
+        datasource.setPortNumber(portnumber);
+        
+        try {
+            cnx = datasource.getConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(" Get Connection -> " + My_CNX.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return cnx;
+    }
+    
+}
